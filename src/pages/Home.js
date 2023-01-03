2232ea42f50b922a88ed513e11c7357a554e311e
Bawandeep Singh <Singhbawan48@gmail.com>
2023-01-03 10:56:07 -0500

    home flex position

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,23 +4,31 @@ import myResume from "../assets/myResume.pdf";
 function Home() {
   return (
     <div className="home__wrapper">
-      <div>
+      <div className="mr-10">
         <p className="home_subtext">Hello, I’m</p>
         <h1 className="home_text">BAWANDEEP SINGH</h1>
         <p className="home_subtext">CREATIVE REACT DEVELOPER</p>
         <p className="home_subtext">BASED IN TORONTO</p>
 
         <div>
-        <a href={myResume} download="Bawandeep Resume">
-          <button className="btn">Download Resume</button>
- </a>
+          <a href={myResume} download="Bawandeep Resume">
+            <button className="btn">Download Resume</button>
+          </a>
         </div>
       </div>
 
-      <div >
+      <div className="ml-10">
         <div className="home_img_Container">
-          <img className="home_img_circle" src="./images/flower_back.jpg" alt="" />
-          <img className="home_img_person" src="./images/Bawandeep_Singh.PNG" alt="Bawandeep Smiling" />
+          <img
+            className="home_img_circle"
+            src="./images/flower_back.jpg"
+            alt=""
+          />
+          <img
+            className="home_img_person"
+            src="./images/Bawandeep_Singh.PNG"
+            alt="Bawandeep Smiling"
+          />
         </div>
       </div>
     </div>
