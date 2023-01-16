d9b434f2808d6232d4e697431fe7f25ce07a6eb8
Bawandeep Singh <Singhbawan48@gmail.com>
2023-01-16 12:56:11 -0500

    project details added

--- src/pages/work.js
+++ src/pages/work.js
@@ -4,33 +4,70 @@ import Logo from "../components/icons";
 let projectData = [
   {
     name: "StarShip",
-    description: "An app to visualize the market data",
+    description:
+      "An app to visualize the market data with a social page to post baskets",
     img: "starship.png",
-    tech: ["react", "css", "mui", "javascript", "mui"],
+    tech: [
+      "javascript",
+      "react",
+      "mongodb",
+      "express",
+      "mui",
+      "node",
+      "graphql",
+      "heroku",
+      "figma",
+      "stripe",
+    ],
     github: "https://github.com/FOR-TIMI/Starship",
     live: "https://still-cliffs-68409.herokuapp.com",
+    figma:
+      "https://www.figma.com/file/21tkx8YyIjDWSJVLL1ySAh/Project-3?node-id=0%3A1&t=4XpdqnB78pVRmX6A-0",
   },
   {
     name: "Join Us",
-    description: "Lorem",
+    description:
+      "This app allows users to post event they are hosting and see the attendees and their comments.",
     img: "Join_Us.png",
-    tech: ["react", "css", "mui", "javascript", "mui"],
+    tech: [
+      "handlebars",
+      "tailwind",
+      "node",
+      "express",
+      "javascript",
+      "mysql",
+      "sequelize",
+      "jest",
+      "mvc",
+      "heroku",
+      "figma",
+    ],
+    github: "https://github.com/nab-man/Join-Us",
+    live: "https://join-us2.herokuapp.com/dashboard",
+    figma:
+      "https://www.figma.com/file/LkWZG25MkJjkwAXnxfipno/Project-2?node-id=0%3A1&t=2wVCRmQk9brvcCpE-1",
+  },
+  {
+    name: "Reactor",
+    description: "These are small games that I made to learn more about react ",
+    img: "reactor.png",
+    tech: ["react", "css", "javascript", "githubpages", "html"],
+    github: "https://github.com/singhbawan/reactor",
+    live: "https://singhbawan.github.io/reactor/",
+  },
+  {
+    name: "Portfolio",
+    description: "The portfolio website that you are visiting.",
+    img: "portfolio.png",
+    tech: ["html", "css", "javascript", "react"],
     github: "https://github.com/FOR-TIMI/Starship",
     live: "https://still-cliffs-68409.herokuapp.com",
   },
   {
     name: "Pods and Parks",
-    description: "Podcast",
+    // description: "Podcast",
     img: "podsandparks.png",
-    tech: ["react", "css", "mui", "javascript", "mui"],
-    github: "https://github.com/FOR-TIMI/Starship",
-    live: "https://still-cliffs-68409.herokuapp.com",
-  },
-  {
-    name: "Reactor",
-    description: "An app to visualize the market data",
-    img: "reactor.png",
-    tech: ["react", "css", "mui", "javascript", "mui"],
+    tech: ["react", "css", "bulma", "javascript"],
     github: "https://github.com/FOR-TIMI/Starship",
     live: "https://still-cliffs-68409.herokuapp.com",
   },
@@ -46,28 +83,35 @@ function Work() {
         {projectData.map((project) => {
           return (
             <li className="project_wrapper" key={project.name}>
+              
               <div className="laptop_container">
                 <img
-                  className="asome"
+                  className="work_project_img"
                   src={`./images/My_work/${project.img}`}
                   alt={`screenshot of ${project.name}`}
                 />
+                
               </div>
               <div className="work_data_container">
-                <h2 className="work_heading-h2">{project.name}</h2>
-                <p className="work_p">{project.description} </p>
-
-                {project.tech.map((icon) => {
-                  return <Logo variant={icon} />;
-                })}
-
-                <div className="flex">
+                <h2 className="work_project_heading mb-1">{project.name}</h2>
+                <p className="work_project_para mb-1 ">{project.description} </p>
+                <div className="flex mb-1 ">
+                  {project.tech.map((icon) => {
+                    return <Logo variant={icon} height={25} />;
+                  })}
+                </div>
+                <div className="flex mb-1 work_project_links">
                   <a href={project.github} target="_blank">
                     <div className="btn">Github</div>
                   </a>
                   <a href={project.live} target="_blank">
                     <div className="btn">Live</div>
                   </a>
+                  {project.figma ? (
+                    <a href={project.figma} target="_blank">
+                      <div className="btn ">Figma</div>
+                    </a>
+                  ) : null}
                 </div>
               </div>
             </li>
