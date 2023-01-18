439c7963fa9120229d47f318f08d53b69b1921ca
Bawandeep Singh <Singhbawan48@gmail.com>
2023-01-18 04:00:49 -0500

    about me page completed

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,26 +1,38 @@
+import "./about.css";
+
 function About() {
   return (
-    <div >
-      <section>
-        <div>
-          <h1>
-            Hi,<br></br>
-            I'm{" "}
-            <span>
-              <img src="./images/LOGO-4.png" alt="B"></img>
-              awandeep Singh,
-            </span>
+    <div>
+      <section className="about_wrapper">
+        <div >
+          <div className="about_text">
+            <p>
+              Hello! My name is Bawandeep Singh, Full-stack web developer with
+              past experience of Machine Builder for 3+ years, having
+              successfully delivered two machines to happy customers.
+            </p>
             <br></br>
-            web developer
-          </h1>
-
-          <p>Fullstack Developer / MERN Stack</p>
-
-          <a href="/contact">Contact me!</a>
+            <p>
+              I enjoy creating things that live on the internet. My interest in
+              programming started back in 2012 during my college where i learned
+              programming robots and PLC's.
+            </p>
+            <br></br>
+            <p>
+              My love for programming developed in college during a
+              competition where I facilitated communication between PLC and
+              Raspberry Pi using Modbus. That was the first time I had to go
+              through a lot documentation to understand the protocol and then
+              program the controllers to communicate with each other. It was
+              exhilarating when it worked, Since then I have always pursued to
+              expand my knowledge.
+            </p>
+          </div>
         </div>
         <img
-          src="./profile pic.jpg"
-          alt="Black and white profile of Bawandeep smiling"
+          className="about_img"
+          src="./images/Profile_pic-full1.jpg"
+          alt="Bawandeep's picture"
         ></img>
       </section>
     </div>
