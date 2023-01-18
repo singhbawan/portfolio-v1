ef73404493c2caba388b680df78fdb307dbace02
Bawandeep Singh <Singhbawan48@gmail.com>
2023-01-18 04:12:24 -0500

    work drop down animation completed

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,77 +1,8 @@
 import "./work.css";
 import Logo from "../components/icons";
+import projectData from "../utils/projectData";
 
-let projectData = [
-  {
-    name: "StarShip",
-    description:
-      "An app to visualize the market data with a social page to post baskets",
-    img: "starship.png",
-    tech: [
-      "javascript",
-      "react",
-      "mongodb",
-      "express",
-      "mui",
-      "node",
-      "graphql",
-      "heroku",
-      "figma",
-      "stripe",
-    ],
-    github: "https://github.com/FOR-TIMI/Starship",
-    live: "https://still-cliffs-68409.herokuapp.com",
-    figma:
-      "https://www.figma.com/file/21tkx8YyIjDWSJVLL1ySAh/Project-3?node-id=0%3A1&t=4XpdqnB78pVRmX6A-0",
-  },
-  {
-    name: "Join Us",
-    description:
-      "This app allows users to post event they are hosting and see the attendees and their comments.",
-    img: "Join_Us.png",
-    tech: [
-      "handlebars",
-      "tailwind",
-      "node",
-      "express",
-      "javascript",
-      "mysql",
-      "sequelize",
-      "jest",
-      "mvc",
-      "heroku",
-      "figma",
-    ],
-    github: "https://github.com/nab-man/Join-Us",
-    live: "https://join-us2.herokuapp.com/dashboard",
-    figma:
-      "https://www.figma.com/file/LkWZG25MkJjkwAXnxfipno/Project-2?node-id=0%3A1&t=2wVCRmQk9brvcCpE-1",
-  },
-  {
-    name: "Reactor",
-    description: "These are small games that I made to learn more about react ",
-    img: "reactor.png",
-    tech: ["react", "css", "javascript", "githubpages", "html"],
-    github: "https://github.com/singhbawan/reactor",
-    live: "https://singhbawan.github.io/reactor/",
-  },
-  {
-    name: "Portfolio",
-    description: "The portfolio website that you are visiting.",
-    img: "portfolio.png",
-    tech: ["html", "css", "javascript", "react"],
-    github: "https://github.com/FOR-TIMI/Starship",
-    live: "https://still-cliffs-68409.herokuapp.com",
-  },
-  {
-    name: "Pods and Parks",
-    // description: "Podcast",
-    img: "podsandparks.png",
-    tech: ["react", "css", "bulma", "javascript"],
-    github: "https://github.com/FOR-TIMI/Starship",
-    live: "https://still-cliffs-68409.herokuapp.com",
-  },
-];
+
 
 function Work() {
   return (
@@ -83,6 +14,20 @@ function Work() {
         {projectData.map((project) => {
           return (
             <li className="project_wrapper" key={project.name}>
+              <input
+                className="work_checkbox"
+                type="checkbox"
+                name="checkbox"
+                id={project.name}
+                value="value"          
+              ></input>
+              <label className="work_label-arrow cursor-pointer" for={project.name}>               
+                <img
+                 
+                  height={75}
+                  src="./images/arrow_down.svg"
+                ></img>
+              </label>
               
               <div className="laptop_container">
                 <img
@@ -90,11 +35,13 @@ function Work() {
                   src={`./images/My_work/${project.img}`}
                   alt={`screenshot of ${project.name}`}
                 />
-                
               </div>
+
               <div className="work_data_container">
                 <h2 className="work_project_heading mb-1">{project.name}</h2>
-                <p className="work_project_para mb-1 ">{project.description} </p>
+                <p className="work_project_para mb-1 ">
+                  {project.description}{" "}
+                </p>
                 <div className="flex mb-1 ">
                   {project.tech.map((icon) => {
                     return <Logo variant={icon} height={25} />;
@@ -114,6 +61,9 @@ function Work() {
                   ) : null}
                 </div>
               </div>
+              <label className="work_label-heading cursor-pointer" for={project.name}> 
+              <p className="work_project_heading ">{project.name}</p>
+              </label>
             </li>
           );
         })}
