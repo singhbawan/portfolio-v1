a5f81c6ea7b1c8c968614ccd3c2889b3385a4c2a
Bawandeep Singh <Singhbawan48@gmail.com>
2023-01-19 10:09:14 -0500

    skills list created

---
+++ src/utils/skillsList.js
@@ -0,0 +1,29 @@
+let skillList = {
+  frontend: [
+    "css",
+    "html",
+    "react",
+    "tailwind",
+    "mui",
+    "bulma",
+    "handlebars",
+    "figma",
+  ],
+  backend: [
+    "javascript",
+    "node",
+    "express",
+    "mongodb",
+    "mysql",
+    "graphql",
+    "sequelize",
+    "heroku",
+    "netlify",
+    "stripe",
+    "mvc",
+    "jest",
+    "githubpages",
+  ],
+};
+
+export default skillList;
