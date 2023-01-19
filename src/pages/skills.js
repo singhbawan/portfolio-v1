f9efa6f5ad173ccf163afa4e79fe8f7e28bbbbde
Bawandeep Singh <Singhbawan48@gmail.com>
2023-01-19 10:09:23 -0500

    skills page created

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,10 +1,30 @@
+import Logo from "../components/icons";
+import "./skills.css";
+import skillList from "../utils/skillsList";
+
 function Skills() {
-    return (
-      <div >
-          <h1>This is Skills page</h1>
+  return (
+    <div className="skills_wrapper">
+      
+      <h1 className="skills_heading">Skills</h1>
+     
+      <div className="skills_container">
+        <section className="skills_items">
+          <h1 className="skills_subheading">Frontend</h1>
+          {skillList.frontend.map((skill) => {
+            return <Logo variant={skill} />;
+          })}
+        </section>
+        <section className="skills_items">
+          <h1 className="skills_subheading">Backend</h1>
+
+          {skillList.backend.map((skill) => {
+            return <Logo variant={skill} />;
+          })}
+        </section>
       </div>
-    );
-  }
-  
-  export default Skills;
-  
+    </div>
+  );
+}
+
+export default Skills;
